3c38220f1001014142e002cb48048d0ff811d260
spz2020 <74664513+spz2020@users.noreply.github.com>
2025-02-05 21:37:18 +0800

    Update Matchmaking.cs

--- Supercell.Laser.Server/Logic/Game/Matchmaking.cs
+++ Supercell.Laser.Server/Logic/Game/Matchmaking.cs
@@ -102,7 +102,7 @@
         public int SecondsLeft;
         private int Turns;
 
-        public const int SEARCH_TIMEOUT = 60;
+        public const int SEARCH_TIMEOUT = 3600;
 
         public MatchmakingSlot(EventData eventData, int playersRequired)
         {
